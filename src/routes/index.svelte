fbee8bfc132d12a85a33139a7d14d09b6e056901
tonyspunei <tony.spunei@yahoo.com>
2022-03-10 00:32:23 +0100

    add search filter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,18 @@
   import PokemanCard from "../components/pokemanCard.svelte";
 
   let searchTerm = "";
+  let filteredPokemon = [];
+
+  $: {
+    if (searchTerm) {
+      // search Pokemon
+      filteredPokemon = $pokemon.filter((pokeman) =>
+        pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      filteredPokemon = [...$pokemon];
+    }
+  }
 </script>
 
 <svelte:head>
@@ -10,8 +22,15 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit</h1>
+
+<input
+  type="text"
+  placeholder="Search Pokemon"
+  class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+  bind:value="{searchTerm}" />
+
 <div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
-  {#each $pokemon as pokeman}
+  {#each filteredPokemon as pokeman}
     <PokemanCard pokeman="{pokeman}" />
   {/each}
 </div>
