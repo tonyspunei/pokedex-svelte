43f6184d64b3d5ca4e1d7607722fde2ec6cdf8bb
tonyspunei <tony.spunei@yahoo.com>
2022-03-09 23:03:51 +0100

    init git

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { pokemon } from "../stores/pokestore";
+    console.log($pokemon)
+</script>
+
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Svelte Kit</h1>
