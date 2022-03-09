a8f6122c481cd35117634db23c2f44fe27a135cc
tonyspunei <tony.spunei@yahoo.com>
2022-03-10 00:05:59 +0100

    add pokemanCard styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,14 @@
 <script>
-    import Nav from "../components/Nav.svelte"
+  import Nav from "../components/Nav.svelte";
 </script>
 
 <div class="p-8 max-w-6xl mx-auto">
-    <Nav /> 
-    <slot></slot>
+  <Nav />
+  <slot />
 </div>
+
 <style>
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script>
-    import { pokemon } from "../stores/pokestore";
+  import { pokemon } from "../stores/pokestore";
+  import PokemanCard from "../components/pokemanCard.svelte";
+
+  let searchTerm = "";
 </script>
 
 <svelte:head>
-    <title>Svelte Kit Pokedex</title>
+  <title>Svelte Kit Pokedex</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit</h1>
-{#each $pokemon as pokeman}
-    <p>{pokeman.name}</p>
-{/each}
- 
+<div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
+  {#each $pokemon as pokeman}
+    <PokemanCard pokeman="{pokeman}" />
+  {/each}
+</div>
